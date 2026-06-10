a518ab6fdddc7190191d10dcd0551b9c23396b70
vithorhugo <hugovithor02@gmail.com>
2026-06-09 23:07:01 -0300

    scrap do numero de paginas concluida

--- app/service/html_letterboxd.py
+++ app/service/html_letterboxd.py
@@ -1,25 +1,19 @@
-# %%
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
 
 def get_html_user_films(name: str, page_number: int,**kwargs):
-
     url = f'https://letterboxd.com/{name}/diary/films/page/{page_number}/'
-    
     options = Options()
     options.add_argument('--headless')
-
     driver = webdriver.Firefox(options=options)
-
     driver.get(url)
-
     time.sleep(10)
 
     resp = driver.page_source
 
     if "Letterboxd - Not Found" in resp:
-        
+
         driver.quit()
 
         return "Usuario não encontrado"
@@ -33,16 +27,24 @@ def get_html_user_films(name: str, page_number: int,**kwargs):
         return resp
 
 def get_html_film(name: str, **kwargs):
-
     url = f'https://letterboxd.com/film/{name}/' 
+    options = Options()
+    options.add_argument('--headless')
 
-    resp = requests.get(url,params=kwargs)
+    driver = webdriver.Firefox(options=options)
 
-    if resp.status_code != 200:
-        return "Filme não encontrado"
+    driver.get(url)
+    time.sleep(10)
+
+    resp = driver.page_source
+
+    if "Letterboxd - Not Found" in resp:
+        driver.quit()
+
+        return "Usuario não encontrado"
+
     else:
-        return resp.text
+        resp = driver.page_source
 
-
-get_html_user_films('a',1)
-# %%
+        driver.quit()
+        return resp
